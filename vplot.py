2c15dd6afef84e42df9b414e7be1a49ed67851b6
victorcchan <chan@astro.utoronto.ca>
2020-10-16 15:54:15 -0400

    Test

--- vplot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib as mpl
-
-mpl.rc('text', usetex = True, color='k')
-mpl.rc('font', family = 'serif')
-mpl.rc('figure', figsize=(12,8))
-mpl.rc('xtick', labelsize=16, c='k')
-mpl.rc('ytick', labelsize=16, c='k')
-mpl.rc('axes', ec='k', titlesize=24, labelsize=20, labelcolor='k')
-mpl.rc('legend', fontsize=16)
